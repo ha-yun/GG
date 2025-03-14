17e0290c449d86be88611a3e1a1d39099cc3f044
ha-yun <deu05230@naver.com>
2025-03-14 17:03:17 +0900

    test

--- products/src/main/java/com/example/products/controller/CustomGoodsController.java
+++ products/src/main/java/com/example/products/controller/CustomGoodsController.java
@@ -2,7 +2,6 @@ package com.example.products.controller;
 
 import com.example.products.dto.CustomGoodsDto;
 import com.example.products.service.CustomGoodsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +9,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/customgoods")
-@RequiredArgsConstructor
 public class CustomGoodsController {
+
     private final CustomGoodsService customGoodsService;
+
+    public CustomGoodsController(CustomGoodsService customGoodsService) {
+        this.customGoodsService = customGoodsService;
+    }
 
     @PostMapping("/save")
     public ResponseEntity<String> saveCustomGoods(
